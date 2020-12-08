01de92818fac7cfaafb1fc6369cc54ee7e701346
thisisaaronland <devnull@localhost>
2020-12-08 12:00:19 -0500

    more flag refactoring

--- flags/flags.go
+++ flags/flags.go
@@ -13,6 +13,17 @@ import (
 	"strings"
 )
 
+func NewFlagSet(name string) *flag.FlagSet {
+
+	fs := flag.NewFlagSet(name, flag.ExitOnError)
+
+	fs.Usage = func() {
+		fs.PrintDefaults()
+	}
+
+	return fs
+}
+
 func Parse(fs *flag.FlagSet) {
 
 	args := os.Args[1:]
@@ -48,70 +59,4 @@ func SetFromEnv(fs *flag.FlagSet) {
 		}
 
 	})
-}
-
-func NewFlagSet(name string) *flag.FlagSet {
-
-	fs := flag.NewFlagSet(name, flag.ExitOnError)
-
-	fs.Usage = func() {
-		fs.PrintDefaults()
-	}
-
-	return fs
-}
-
-func CommonFlags() (*flag.FlagSet, error) {
-
-	fs := NewFlagSet("common")
-
-	// spatial databases
-
-	availables_databases := database.Schemes()
-	desc_databases := fmt.Sprintf("Valid options are: %s", available_databases)
-
-	fs.String("spatial-database-uri", "rtree://", desc_databases)
-
-	// property readers
-
-	fs.Bool("enable-properties", false, "Enable support for 'properties' parameters in queries.")
-
-	availables_property_readers := properties.Schemes()
-	desc_property_readers := fmt.Sprintf("Valid options are: %s", available_property_readers)
-
-	fs.String("properties-reader-uri", "", desc_property_readers)
-
-	// indexing modes
-
-	modes := index.Modes()
-	sort.Strings(modes)
-
-	valid_modes := strings.Join(modes, ", ")
-	desc_modes := fmt.Sprintf("Valid modes are: %s.", valid_modes)
-
-	fs.String("mode", "repo://", desc_modes)
-
-	//
-
-	fs.Bool("is-wof", true, "Input data is WOF-flavoured GeoJSON. (Pass a value of '0' or 'false' if you need to index non-WOF documents.")
-
-	fs.Bool("enable-custom-placetypes", false, "...")
-	fs.String("custom-placetypes-source", "", "...")
-	fs.String("custom-placetypes", "", "...")
-
-	// this is invoked/used in app/indexer.go but for the life of me I can't
-	// figure out how to make the code in flags/exclude.go implement the
-	// correct inferface wah wah so that flag.Lookup("exclude").Value returns
-	// something we can loop over... so instead we just strings.Split() on
-	// flag.Lookup("exclude").String() which is dumb but works...
-	// (20180301/thisisaaronland)
-
-	var exclude Exclude
-	fs.Var(&exclude, "exclude", "Exclude (WOF) records based on their existential flags. Valid options are: ceased, deprecated, not-current, superseded.")
-
-	fs.Bool("setenv", false, "Set flags from environment variables.")
-	fs.Bool("verbose", false, "Be chatty.")
-	fs.Bool("strict", false, "Be strict about flags and fail if any are missing or deprecated flags are used.")
-
-	return fs, nil
 }
