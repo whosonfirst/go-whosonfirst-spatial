ef93d4a58ee9d9ede0fb77588af30a52f028a9f9
thisisaaronland <devnull@localhost>
2021-10-07 08:26:26 -0700

    update SpatialIndex interface to pass orb.Point as pointer

--- spatial.go
+++ spatial.go
@@ -10,10 +10,10 @@ import (
 type SpatialIndex interface {
 	IndexFeature(context.Context, []byte) error
 	RemoveFeature(context.Context, string) error
-	PointInPolygon(context.Context, orb.Point, ...Filter) (spr.StandardPlacesResults, error)
-	PointInPolygonCandidates(context.Context, orb.Point, ...Filter) ([]*PointInPolygonCandidate, error)
-	PointInPolygonWithChannels(context.Context, chan spr.StandardPlacesResult, chan error, chan bool, orb.Point, ...Filter)
-	PointInPolygonCandidatesWithChannels(context.Context, chan *PointInPolygonCandidate, chan error, chan bool, orb.Point, ...Filter)
+	PointInPolygon(context.Context, *orb.Point, ...Filter) (spr.StandardPlacesResults, error)
+	PointInPolygonCandidates(context.Context, *orb.Point, ...Filter) ([]*PointInPolygonCandidate, error)
+	PointInPolygonWithChannels(context.Context, chan spr.StandardPlacesResult, chan error, chan bool, *orb.Point, ...Filter)
+	PointInPolygonCandidatesWithChannels(context.Context, chan *PointInPolygonCandidate, chan error, chan bool, *orb.Point, ...Filter)
 	Disconnect(context.Context) error
 }
 
